6dcaaa014dc884fa624d829ce9b019e0e05f298f
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-14 12:50:27 +0900

    Title: 다양하게 출력, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240114/다양하게 출력/print-in-variety.py
+++ 240114/다양하게 출력/print-in-variety.py
@@ -1,5 +1,5 @@
 from math import *
-print("Total days in Year")
-print(int(365))
-print("Circumference rate")
-print(round(pi * (10 ** 10) / (10 ** 10), 10))
+print('''Total days in Year
+365
+Circumference rate''')
+print(3.1415926535)
