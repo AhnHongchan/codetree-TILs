05fbceddcf3dd566a4d9de94c0e1c02a120b587c
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-23 14:35:23 +0900

    Title: 좀 더 어려운 수학 점수, Time: 179ms, Memory: 73MB, Status: Passed - CodeTree

--- 240123/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
+++ 240123/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -1,14 +1,7 @@
 math_a, eng_a = map(int, input().split())
 math_b, eng_b = map(int, input().split())
 
-if math_a > math_b:
-    print('A')
-elif math_a < math_b:
-    print('B')
-elif math_a == math_b:
-    if eng_a > eng_b:
-        print('A')
-    elif eng_a < eng_b:
-        print('B')
-    else:
-        pass
+if math_a > math_b or (math_a == math_b and eng_a > eng_b):
+    print("A")
+else:
+    print("B")
