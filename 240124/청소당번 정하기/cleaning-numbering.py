4a024cae074a5c4f1db273398d464ba02e53d814
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-24 00:22:18 +0900

    Title: 청소당번 정하기, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240124/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,14 @@
+num = int(input())
+a = 0
+b = 0
+c = 0
+for i in range(1, num+1):
+    if i % 12 == 0:
+        c += 1
+    elif i % 3 == 0:
+        b += 1
+    elif i % 2 == 0:
+        a += 1
+
+
+print(a, b, c)
