9dba8b8b38b36416c19727054293690486a5aca6
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-23 23:58:02 +0900

    Title: a 부터 b 까지, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240123/a 부터 b 까지/a-to-b.py
+++ 240123/a 부터 b 까지/a-to-b.py
@@ -1,8 +1,8 @@
 a, b = map(int, input().split())
-while a < b:
-    if a % 2 == 0:
-        print(a, end=" ")
-        a += 3
-    if a % 2 == 1:
-        print(a, end=" ")
-        a *= 2
+i = a
+while i <= b:
+	print(i, end=" ")
+	if i % 2 == 1:
+		i *= 2
+	else:
+		i += 3
