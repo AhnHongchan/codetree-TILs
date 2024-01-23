e3bc08396f2e791abce63dbf1e182819c6ef9fe7
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-24 00:04:13 +0900

    Title: 369 게임, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/369 게임/369-game.py
+++ 240124/369 게임/369-game.py
@@ -3,6 +3,10 @@ i = 1
 while i <= num:
     if i % 3 == 0:
         print(0, end = " ")
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print(0, end = " ")
+    elif i // 10 == 3 or i// 10 == 6 or i // 10 == 9:
+        print(0, end = " ")
     else:
         print(i, end = " ")
     i += 1
