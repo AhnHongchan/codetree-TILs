d260301b1777850243fdb9391a6d791afaac72ea
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-24 16:20:44 +0900

    Title: 조건에 부합하는 수, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240124/조건에 부합하는 수/number-that-meets-the-condition.py
+++ 240124/조건에 부합하는 수/number-that-meets-the-condition.py
@@ -2,27 +2,9 @@ num = int(input())
 for i in range(1, num+1):
     if i % 2 == 0 and i % 4 !=0:
         pass
-    elif i // 8 == 0 :
+    elif (i // 8) % 2 == 0 :
         pass
-    elif i // 8 == 2 :
-        pass
-    elif i // 8 == 4 :
-        pass
-    elif i // 8 == 6 :
-        pass
-    elif i // 8 == 8 :
-        pass
-    elif i // 8 == 10 :
-        pass
-    elif i // 8 == 12 :
-        pass
-    elif i % 7 == 0:
-        pass
-    elif i % 7 == 1:
-        pass
-    elif i % 7 == 2:
-        pass
-    elif i % 7 == 3:
+    elif (i % 7) < 4:
         pass
     else:
         print(i, end = " ")
