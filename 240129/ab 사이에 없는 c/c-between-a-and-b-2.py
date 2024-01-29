fc2d723e847ffbdfb64b762024ff6652a8a10bf6
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-29 15:15:45 +0900

    Title: ab 사이에 없는 c, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

--- 240129/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240129/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,12 +1,18 @@
-a, b, c = map(int, input().split())
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
 satisfied = True
-for i in range(a, b+1):
-    if i % c == 0:
-        pass
-    else:
-        satisfied = False
 
+for i in range(a, b + 1):
+	# a에서 b사이의 값 중 c의 배수가 있는지 확인합니다.
+	if i % c == 0:
+		satisfied = False
+
+
+# 출력
 if satisfied == True:
-    print('NO')
+	print("YES")
 else:
-    print('YES')
+	print("NO")
