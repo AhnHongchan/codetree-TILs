976e6b7fb51abd371b3ecf6cf7da3adb908f4dfe
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-01-30 11:44:52 +0900

    Title: 두 개의 직각삼각형, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240130/두 개의 직각삼각형/two-right-triangle.py
+++ 240130/두 개의 직각삼각형/two-right-triangle.py
@@ -3,10 +3,7 @@ for i in range(num):
     for j in range(num-i):
         print('*', end ="")
 
-    for k in range(i):
-        print(" ", end = "")
-    
-    for k in range(i):
+    for k in range(2*i):
         print(" ", end = "")
     
     for j in range(num-i):
