ea65f6a3fbd153b6df81b6ad211cd47c0284c30c
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-01 16:58:22 +0900

    Title: 별표 출력하기 11, Time: 150ms, Memory: 24MB, Status: Passed - CodeTree

--- 240201/별표 출력하기 11/print-star-11.py
+++ 240201/별표 출력하기 11/print-star-11.py
@@ -2,11 +2,11 @@ n = int(input())
 
 for i in range(2*n+1):
     if i % 2 == 0:
-        for j in range(7):
+        for j in range(2 * n + 1):
             print('*', end = ' ')
 
     if i % 2 == 1:
-        for j in range(4):
+        for j in range(n+1):
             print('*  ', end = ' ')
     
     print()
