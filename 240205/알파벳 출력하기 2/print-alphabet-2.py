941447a7f29ee4cd12ea740962ee0d80520eaae1
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-05 17:54:22 +0900

    Title: 알파벳 출력하기 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240205/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,9 @@
+num = int(input())
+cnt = 0
+for i in range(num):
+    for j in range(i):
+        print(' ', end=' ')
+    for j in range(num-i, 0, -1):
+        print(chr(65+cnt), end=' ')
+        cnt += 1
+    print()
