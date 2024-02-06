56c29c7720588b9da97de77169131806f2ef45c3
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-06 15:11:24 +0900

    Title: 배열의 특정 위치 찾기, Time: 119ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240206/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240206/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -2,7 +2,8 @@ numbers = list(map(int, input().split()))
 sum2 = 0
 cnt = 0
 sum3 = 0
-for i in range(0, len(numbers)):
+
+for i in range(len(numbers)):
     if (i+1) % 2 == 0:
         sum2 += numbers[i]
     elif (i+1) % 3 == 0:
@@ -11,4 +12,4 @@ for i in range(0, len(numbers)):
 
 avg = sum3 / cnt
 
-print(sum2, f'{avg:.1f}')
+print(f'{sum2} {avg:.1f}')
