7846038cf8aa8f4714c94e8d97599424e224096c
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-06 15:12:43 +0900

    Title: 배열의 특정 위치 찾기, Time: 106ms, Memory: 24MB, Status: Passed - CodeTree

--- 240206/배열의 특정 위치 찾기/find-specific-location-fo-array.py
+++ 240206/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -1,15 +1,18 @@
-numbers = list(map(int, input().split()))
+# 배열에 주어진 수를 입력받아 저장합니다.
+arr = list(map(int, input().split()))
 sum2 = 0
-cnt = 0
 sum3 = 0
+cnt = 0
 
-for i in range(len(numbers)):
-    if (i+1) % 2 == 0:
-        sum2 += numbers[i]
-    elif (i+1) % 3 == 0 and i != 0:
-        sum3 += numbers[i]
-        cnt += 1
+# 짝수 번째 인덱스에 들어있는 수들의 합과 3의 배수 번째 인덱스에 들어있는 수들의 평균을 구합니다.
+for i in range(10):
+	if (i + 1) % 2 == 0:
+		sum2 += arr[i]
+	if (i + 1) % 3 == 0:
+		sum3 += arr[i]
+		cnt += 1
 
-avg = sum3 / cnt
+# 3의 배수 번째 인덱스에 들어있는 수들의 평균 구하기
+avg3 = sum3 / cnt
 
-print(f'{sum2} {avg:.1f}')
+print(f"{sum2} {avg3:.1f}")
