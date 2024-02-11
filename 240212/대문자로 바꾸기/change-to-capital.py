68bc566e8699cbc61f329dd46cbe9c9d7e57910a
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-12 01:52:37 +0900

    Title: 대문자로 바꾸기, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/대문자로 바꾸기/change-to-capital.py
+++ 240212/대문자로 바꾸기/change-to-capital.py
@@ -2,5 +2,5 @@ small = [list(input().split()) for _ in range(5)]
 
 for i in range(5):
     for j in range(3):
-        print(small[i][j])
+        print(chr(ord(small[i][j])-32), end=' ')
     print()
