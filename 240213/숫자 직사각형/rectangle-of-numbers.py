127b9e160ce424056be5c747c973b45d9dd80ad9
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-13 14:34:44 +0900

    Title: 숫자 직사각형, Time: 134ms, Memory: 25MB, Status: Passed - CodeTree

--- 240213/숫자 직사각형/rectangle-of-numbers.py
+++ 240213/숫자 직사각형/rectangle-of-numbers.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 lst = [[m*i+j+1 for j in range(m)] for i in range(n)]
 
-for i in range(n):
-    for j in range(m):
-        print(lst[i][j], end = ' ')
+for x in lst:
+    for y in x:
+        print(y, end=' ')
     print()
