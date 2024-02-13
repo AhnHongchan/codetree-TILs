21ff4eb14386207ff015e12addf4c58a93ce0719
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-13 17:12:41 +0900

    Title: 지그재그로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240213/지그재그로 숫자 채우기/zigzag-numbering.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+lst = [[0 for _ in range(m)] for _ in range(n)]
+cnt = 0
+
+for j in range(m):
+    for i in range(n):
+        if j % 2 == 0:
+            lst[i][j] = cnt
+            cnt += 1
+        else:
+            lst[3-i][j] = cnt
+            cnt += 1
+
+for x in lst:
+    for elem in x:
+        print(elem, end=' ')
+    print()
