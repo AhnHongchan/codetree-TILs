7fb89ae3685380285388d57f4899ba026a5d76cb
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-16 14:11:09 +0900

    Title: Run Length 인코딩, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240216/Run Length 인코딩/run-length-encoding.py
@@ -0,0 +1,20 @@
+txt = input()
+encoded = ""
+
+curr_char = txt[0]
+num_char = 1
+for target_char in txt[1:]:
+    if target_char == curr_char:
+        num_char += 1
+    else:
+        encoded += curr_char
+        encoded += str(num_char)
+
+        curr_char = target_char
+        num_char = 1
+
+encoded += curr_char
+encoded += str(num_char)
+
+print(len(encoded))
+print(encoded)
