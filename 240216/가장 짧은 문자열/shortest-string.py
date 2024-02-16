c1b7c7ec8d87cbbf7dda72e8087a065c85498728
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-16 09:43:32 +0900

    Title: 가장 짧은 문자열, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 240216/가장 짧은 문자열/shortest-string.py
+++ 240216/가장 짧은 문자열/shortest-string.py
@@ -6,7 +6,8 @@ for i in range(3):
     lst.append(length)
 
 for i in range(2):
-    if lst[i] > lst[i+1]:
-        lst[i], lst[i+1] = lst[i+1], lst[i]
+    for j in range(i+1, 3):
+        if lst[i] > lst[j]:
+            lst[i], lst[j] = lst[j], lst[i]
 
 print(lst[2] - lst[0])
