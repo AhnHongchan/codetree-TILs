7db3569dbe8502c76c95aa0d7822eb8a369e532c
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-02-16 12:35:01 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 250ms, Memory: 24MB, Status: Passed - CodeTree

--- 240216/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 240216/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -3,7 +3,12 @@ for i in range(10):
     txt.append(input())
 
 char = input()
+cnt = 0
 
 for i in range(10):
     if txt[i][-1] == char:
         print(txt[i])
+        cnt += 1
+
+if cnt == 0:
+    print('None')
