5e3b5a6fbb3c8e04e52f9f5d6ce82fa3565eab30
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-07 23:03:22 +0900

    Title: 2021년 날짜의 유무, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240407/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,17 @@
+M, D = map(int, input().split())
+
+long = [1,3,5,7,8,10,12]
+short = [4,6,9,11]
+
+result = 'No'
+if M in long:
+    if D <= 31:
+        result = 'Yes'
+elif M in short:
+    if D <= 30:
+        result = 'Yes'
+else:
+    if D <= 28:
+        result = 'Yes'
+
+print(result)
