ab4f018664833d132869e50744d986d73286d34d
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 17:49:17 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 150ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240410/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,22 @@
+# 변수 선언 및 입력
+n = int(input())
+arr = list(map(int, input().split()))
+numbers = [
+    (num, i)
+    for i, num in enumerate(arr)
+]
+
+answer = [
+    0 for _ in range(n)
+]
+
+# Custom Comparator를 활용한 정렬
+numbers.sort(key=lambda x: (x[0], x[1]))
+
+# 정렬된 숫자들의 원래 인덱스를 활용한 정답 배열 저장
+for i, (_, index) in enumerate(numbers):
+    answer[index] = i + 1 # 인덱스 보정
+
+# 출력
+for i in range(n):
+    print(answer[i], end = ' ')
