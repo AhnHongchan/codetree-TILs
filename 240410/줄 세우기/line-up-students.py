5be14562e1cb3e626fb1fd2ac4f471d7070e58ca
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 16:02:02 +0900

    Title: 줄 세우기, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240410/줄 세우기/line-up-students.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+lst = []
+for i in range(n):
+    h, w = map(int, input().split())
+    lst.append([h, w, i+1])
+
+lst.sort(key=lambda x: (-x[0], -x[1], x[2]))
+
+for i in range(n):
+    print(*lst[i])
