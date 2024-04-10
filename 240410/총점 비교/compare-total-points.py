bf6b1eb8523f86079c72d126b9cc6aefe2a7693a
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 15:48:35 +0900

    Title: 총점 비교, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240410/총점 비교/compare-total-points.py
@@ -0,0 +1,10 @@
+n = int(input())
+lst = []
+for _ in range(n):
+    name, kor, eng, math = input().split()
+    lst.append([name, kor, eng, math])
+
+lst.sort(key=lambda x:(int(x[1])+int(x[2])+int(x[3])))
+
+for i in range(n):
+    print(*lst[i])
