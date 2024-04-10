2161cf7f01cc0287e47b31fa18f3168ba0b6e635
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 16:18:01 +0900

    Title: 원점으로부터의 거리, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/원점으로부터의 거리/distance-from-origin.py
+++ 240410/원점으로부터의 거리/distance-from-origin.py
@@ -4,6 +4,6 @@ for i in range(n):
     x, y = map(int, input().split())
     lst.append([x, y, i+1])
 
-lst.sort(key=lambda x:((x[0]**2+x[1]**2), x[2]))
+lst.sort(key=lambda x:(abs(x[0]-0)+abs(x[1]-0), x[2]))
 for i in range(n):
     print(lst[i][2])
