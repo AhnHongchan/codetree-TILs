f7973514ac81fd7dcfed53ed49eef7e84e93db1f
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 10:46:43 +0900

    Title: 2개씩 그룹짓기, Time: 136ms, Memory: 25MB, Status: Passed - CodeTree

--- 240410/2개씩 그룹짓기/group-of-pairs.py
+++ 240410/2개씩 그룹짓기/group-of-pairs.py
@@ -1,8 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
+arr.sort()
 max_v = 0
-for i in range(n//2):
-    v = arr[i] + arr[n-1-i]
+for i in range(len(arr)//2):
+    v = arr[i] + arr[len(arr)-1-i]
     if max_v < v:
         max_v = v
 
