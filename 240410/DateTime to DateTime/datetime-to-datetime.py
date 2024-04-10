71bcd2e4a9c9cb122a964d3f3f2fb59629c1d153
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 20:54:11 +0900

    Title: DateTime to DateTime, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/DateTime to DateTime/datetime-to-datetime.py
+++ 240410/DateTime to DateTime/datetime-to-datetime.py
@@ -1,15 +1,28 @@
 a, b, c = map(int, input().split())
 cnt = 0
 
-while True:
-    if a == 11 and b == 11 and c == 11:
-        print(cnt)
-        break
-    c -= 1
-    cnt += 1
-    if c == 0:
-        c = 60
-        b -= 1
-        if b == -1:
-            b = 23
-            a -= 1
+if (a == 11 and b == 11 and c <= 11) or (a == 11 and b <= 10):
+    while True:
+        if a == 11 and b == 11 and c == 11:
+            print(cnt)
+            break
+        c += 1
+        cnt += 1
+        if c == 60:
+            c = 0
+            b += 1
+
+
+else:
+    while True:
+        if a == 11 and b == 11 and c == 11:
+            print(cnt)
+            break
+        c -= 1
+        cnt += 1
+        if c == 0:
+            c = 60
+            b -= 1
+            if b == -1:
+                b = 23
+                a -= 1
