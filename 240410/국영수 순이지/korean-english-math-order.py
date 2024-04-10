caaa2d7fce33a926dda3bde2b01e7a11f335fc4b
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 15:41:57 +0900

    Title: 국영수 순이지, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/국영수 순이지/korean-english-math-order.py
+++ 240410/국영수 순이지/korean-english-math-order.py
@@ -4,7 +4,7 @@ for _ in range(n):
     name, kor, eng, math = input().split()
     lst.append([name, kor, eng, math])
 
-lst.sort(key=lambda x:(x[1], x[2], x[3]), reverse=True)
+lst.sort(key=lambda x:(x[1], x[2], x[3]))
 
 for i in range(n):
-    print(*lst[i])
+    print(*lst[n-1-i])
