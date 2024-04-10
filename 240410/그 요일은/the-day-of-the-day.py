1308eae2b88881a0e08229650246ca44cba0e032
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-10 21:22:00 +0900

    Title: 그 요일은, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240410/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int, input().split())
+day = input()
+cnt = 0
+
+while True:
+    if m1 == m2 and d1 == d2:
+        break
+    cnt += 1
+    d1 += 1
+    if m1 in [1, 3, 5, 7, 8, 10, 12] and d1 == 31:
+        d1 = 0
+        m1 += 1
+    elif m1 in [4, 6, 9, 11] and d1 == 30:
+        d1 = 0
+        m1 += 1
+    elif m1 == 2 and d1 == 29:
+        d1 = 0
+        m1 += 1
+
+cnt //= 7
+print(cnt+1)
