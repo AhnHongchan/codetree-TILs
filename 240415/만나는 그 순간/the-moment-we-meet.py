35165d131eb69739c79201e4f71065824d905804
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-15 18:04:01 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240415/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,32 @@
+N, M = map(int, input().split())
+arr_a = [0]
+arr_b = [0]
+for _ in range(N):
+    d, t = input().split()
+    t = int(t)
+    if d == 'R':
+        for i in range(arr_a[-1]+1, arr_a[-1]+t+1):
+            arr_a.append(i)
+    else:
+        for i in range(arr_a[-1]-1, arr_a[-1]-t-1, -1):
+            arr_a.append(i)
+
+for _ in range(M):
+    d, t = input().split()
+    t = int(t)
+    if d == 'R':
+        for i in range(arr_b[-1]+1, arr_b[-1]+t+1):
+            arr_b.append(i)
+    else:
+        for i in range(arr_b[-1]-1, arr_b[-1]-t-1, -1):
+            arr_b.append(i)
+
+# print(arr_a)
+# print(arr_b)
+
+for i in range(1, len(arr_a)+1):
+    if arr_a[i] == arr_b[i]:
+        print(i)
+        break
+else:
+    print(-1)
