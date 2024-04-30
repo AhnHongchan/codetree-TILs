523d2bd3094f3cb7105fe13db61a285c4847e588
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-30 13:33:34 +0900

    Title: 거울에 레이저 쏘기 2, Time: 157ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240430/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 240430/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -1,104 +1,104 @@
-N = int(input())
-arr = [list(input()) for _ in range(N)]
-K = int(input())
-# 아래 / 왼 / 위 / 오른
-dx, dy = [1,0,-1,0],[0,-1,0,1]
-cnt = 0
+# N = int(input())
+# arr = [list(input()) for _ in range(N)]
+# K = int(input())
+# # 아래 / 왼 / 위 / 오른
+# dx, dy = [1,0,-1,0],[0,-1,0,1]
+# cnt = 0
 
-if 1 <= K < N:
-    x = 0
-    y = K-1
-    d = 0
-    while 0<=x<N and 0<=y<N:
-        if arr[x][y] == '/':
-            d = (d + 1) % 4
-            x = x + dx[d]
-            y = y + dy[d]
-        else:
-            if d == 0:
-                d = 3
-            elif d == 1:
-                d = 2
-            elif d == 2:
-                d = 1
-            elif d == 3:
-                d = 0
+# if 1 <= K < N:
+#     x = 0
+#     y = K-1
+#     d = 0
+#     while 0<=x<N and 0<=y<N:
+#         if arr[x][y] == '/':
+#             d = (d + 1) % 4
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         else:
+#             if d == 0:
+#                 d = 3
+#             elif d == 1:
+#                 d = 2
+#             elif d == 2:
+#                 d = 1
+#             elif d == 3:
+#                 d = 0
             
-            x = x + dx[d]
-            y = y + dy[d]
-        cnt += 1
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         cnt += 1
             
 
-elif N+1 <= K < 2*N:
-    x = K-N-1
-    y = N-1
-    d = 1
-    while 0<=x<N and 0<=y<N:
-        if arr[x][y] == '/':
-            d = (d + 1) % 4
-            x = x + dx[d]
-            y = y + dy[d]
-        else:
-            if d == 0:
-                d = 3
-            elif d == 1:
-                d = 2
-            elif d == 2:
-                d = 1
-            elif d == 3:
-                d = 0
+# elif N+1 <= K < 2*N:
+#     x = K-N-1
+#     y = N-1
+#     d = 1
+#     while 0<=x<N and 0<=y<N:
+#         if arr[x][y] == '/':
+#             d = (d + 1) % 4
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         else:
+#             if d == 0:
+#                 d = 3
+#             elif d == 1:
+#                 d = 2
+#             elif d == 2:
+#                 d = 1
+#             elif d == 3:
+#                 d = 0
             
-            x = x + dx[d]
-            y = y + dy[d]
-        cnt += 1
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         cnt += 1
 
-elif 2*N+1 <= K < 3*N:
-    x = N-1
-    y = 3*N-K
-    d = 2
-    while 0<=x<N and 0<=y<N:
-        if arr[x][y] == '/':
-            d = (d + 1) % 4
-            x = x + dx[d]
-            y = y + dy[d]
-        else:
-            if d == 0:
-                d = 3
-            elif d == 1:
-                d = 2
-            elif d == 2:
-                d = 1
-            elif d == 3:
-                d = 0
+# elif 2*N+1 <= K < 3*N:
+#     x = N-1
+#     y = 3*N-K
+#     d = 2
+#     while 0<=x<N and 0<=y<N:
+#         if arr[x][y] == '/':
+#             d = (d + 1) % 4
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         else:
+#             if d == 0:
+#                 d = 3
+#             elif d == 1:
+#                 d = 2
+#             elif d == 2:
+#                 d = 1
+#             elif d == 3:
+#                 d = 0
             
-            x = x + dx[d]
-            y = y + dy[d]
-        cnt += 1
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         cnt += 1
 
-else:
-    x = 4*N-K
-    y = 0
-    d = 3
-    while 0<=x<N and 0<=y<N:
-        if arr[x][y] == '/':
-            d = (d + 1) % 4
-            x = x + dx[d]
-            y = y + dy[d]
-        else:
-            if d == 0:
-                d = 3
-            elif d == 1:
-                d = 2
-            elif d == 2:
-                d = 1
-            elif d == 3:
-                d = 0
+# else:
+#     x = 4*N-K
+#     y = 0
+#     d = 3
+#     while 0<=x<N and 0<=y<N:
+#         if arr[x][y] == '/':
+#             d = (d + 1) % 4
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         else:
+#             if d == 0:
+#                 d = 3
+#             elif d == 1:
+#                 d = 2
+#             elif d == 2:
+#                 d = 1
+#             elif d == 3:
+#                 d = 0
             
-            x = x + dx[d]
-            y = y + dy[d]
-        cnt += 1
+#             x = x + dx[d]
+#             y = y + dy[d]
+#         cnt += 1
 
-print(cnt)
+# print(cnt)
 
 
 # \
@@ -119,3 +119,98 @@ print(cnt)
 # K = N+1~2N : (K-N-1, N-1)
 # K = 2N+1~3N : (N-1, 3N-K)
 # K = 3N+1~4N : (4N-K, 0)
+
+def change_direction(current_direction, mirror):
+    # 현재 방향이 위쪽인 경우
+    if current_direction == 0:
+        if mirror == '/':
+            return 3  # 반시계방향으로 변경
+        else:
+            return 1  # 시계방향으로 변경
+    
+    # 현재 방향이 왼쪽인 경우
+    elif current_direction == 1:
+        if mirror == '/':
+            return 0
+        else:
+            return 2
+    
+    # 현재 방향이 아래쪽인 경우
+    elif current_direction == 2:
+        if mirror == '/':
+            return 1
+        else:
+            return 3
+    
+    # 현재 방향이 오른쪽인 경우
+    elif current_direction == 3:
+        if mirror == '/':
+            return 2
+        else:
+            return 0
+
+N = int(input())
+arr = [list(input()) for _ in range(N)]
+K = int(input())
+
+# 아래 / 왼 / 위 / 오른
+dx, dy = [1,0,-1,0], [0,-1,0,1]
+cnt = 0
+
+if 1 <= K < N:
+    x = 0
+    y = K-1
+    d = 0
+    while 0 <= x < N and 0 <= y < N:
+        if arr[x][y] == '/':
+            d = change_direction(d, '/')
+        else:
+            d = change_direction(d, '\\')
+        
+        x += dx[d]
+        y += dy[d]
+        cnt += 1
+
+elif N+1 <= K < 2*N:
+    x = K-N-1
+    y = N-1
+    d = 1
+    while 0 <= x < N and 0 <= y < N:
+        if arr[x][y] == '/':
+            d = change_direction(d, '/')
+        else:
+            d = change_direction(d, '\\')
+        
+        x += dx[d]
+        y += dy[d]
+        cnt += 1
+
+elif 2*N+1 <= K < 3*N:
+    x = N-1
+    y = 3*N-K
+    d = 2
+    while 0 <= x < N and 0 <= y < N:
+        if arr[x][y] == '/':
+            d = change_direction(d, '/')
+        else:
+            d = change_direction(d, '\\')
+        
+        x += dx[d]
+        y += dy[d]
+        cnt += 1
+
+else:
+    x = 4*N-K
+    y = 0
+    d = 3
+    while 0 <= x < N and 0 <= y < N:
+        if arr[x][y] == '/':
+            d = change_direction(d, '/')
+        else:
+            d = change_direction(d, '\\')
+        
+        x += dx[d]
+        y += dy[d]
+        cnt += 1
+
+print(cnt)
