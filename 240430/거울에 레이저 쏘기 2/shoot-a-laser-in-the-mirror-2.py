7285df50865e74a99c323ffff2f7f2cc48d00f0d
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-30 13:30:09 +0900

    Title: 거울에 레이저 쏘기 2, Time: 129ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240430/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,121 @@
+N = int(input())
+arr = [list(input()) for _ in range(N)]
+K = int(input())
+# 아래 / 왼 / 위 / 오른
+dx, dy = [1,0,-1,0],[0,-1,0,1]
+cnt = 0
+
+if 1 <= K < N:
+    x = 0
+    y = K-1
+    d = 0
+    while 0<=x<N and 0<=y<N:
+        if arr[x][y] == '/':
+            d = (d + 1) % 4
+            x = x + dx[d]
+            y = y + dy[d]
+        else:
+            if d == 0:
+                d = 3
+            elif d == 1:
+                d = 2
+            elif d == 2:
+                d = 1
+            elif d == 3:
+                d = 0
+            
+            x = x + dx[d]
+            y = y + dy[d]
+        cnt += 1
+            
+
+elif N+1 <= K < 2*N:
+    x = K-N-1
+    y = N-1
+    d = 1
+    while 0<=x<N and 0<=y<N:
+        if arr[x][y] == '/':
+            d = (d + 1) % 4
+            x = x + dx[d]
+            y = y + dy[d]
+        else:
+            if d == 0:
+                d = 3
+            elif d == 1:
+                d = 2
+            elif d == 2:
+                d = 1
+            elif d == 3:
+                d = 0
+            
+            x = x + dx[d]
+            y = y + dy[d]
+        cnt += 1
+
+elif 2*N+1 <= K < 3*N:
+    x = N-1
+    y = 3*N-K
+    d = 2
+    while 0<=x<N and 0<=y<N:
+        if arr[x][y] == '/':
+            d = (d + 1) % 4
+            x = x + dx[d]
+            y = y + dy[d]
+        else:
+            if d == 0:
+                d = 3
+            elif d == 1:
+                d = 2
+            elif d == 2:
+                d = 1
+            elif d == 3:
+                d = 0
+            
+            x = x + dx[d]
+            y = y + dy[d]
+        cnt += 1
+
+else:
+    x = 4*N-K
+    y = 0
+    d = 3
+    while 0<=x<N and 0<=y<N:
+        if arr[x][y] == '/':
+            d = (d + 1) % 4
+            x = x + dx[d]
+            y = y + dy[d]
+        else:
+            if d == 0:
+                d = 3
+            elif d == 1:
+                d = 2
+            elif d == 2:
+                d = 1
+            elif d == 3:
+                d = 0
+            
+            x = x + dx[d]
+            y = y + dy[d]
+        cnt += 1
+
+print(cnt)
+
+
+# \
+# 아래로 내려오면 오른쪽 방향으로 나감
+# 왼쪽 방향으로 들어오면 위로 나감
+# 오른쪽 방향으로 들어오면 아래로 나감
+# 위쪽 방향으로 들어오면 왼쪽으로 나감
+# 즉 아래 - 오른쪽 / 위 - 왼쪽 이렇게 묶을 수 있음
+
+# /
+# 아래 -> 왼
+# 위 -> 오른
+# 왼 -> 위
+# 오른 -> 아래
+# 시계방향으로 돈다
+
+# K = 1 ~ N : (0,K-1)
+# K = N+1~2N : (K-N-1, N-1)
+# K = 2N+1~3N : (N-1, 3N-K)
+# K = 3N+1~4N : (4N-K, 0)
