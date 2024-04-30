195fc084c7a3e307f46989ad01642273106081c1
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-04-30 09:22:43 +0900

    Title: 되돌아오기, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240430/되돌아오기/come-back.py
@@ -0,0 +1,28 @@
+N = int(input())
+x, y = 0, 0
+dx, dy = [0,1,0,-1], [1,0,-1,0]
+polar = {
+    'N': 0,
+    'E': 1,
+    'S': 2,
+    'W': 3,
+}
+
+cnt = 0
+stop = False
+for i in range(N):
+    # d는 방향, w는 거리
+    d, w = input().split()
+    w = int(w)
+    
+    for j in range(w):
+        x += dx[polar[d]]
+        y += dy[polar[d]]
+        cnt += 1
+        if x == 0 and y == 0:
+            stop = True
+            break
+    if stop:
+        break
+
+print(cnt)
