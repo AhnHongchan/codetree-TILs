826ca2b0a2fe6839a3bf16df02ba37c3b0b1bc56
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-07 01:46:57 +0900

    Title: 원 모양으로 되어있는 방, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/원 모양으로 되어있는 방/a-room-in-a-circle.py
+++ 240507/원 모양으로 되어있는 방/a-room-in-a-circle.py
@@ -3,13 +3,13 @@ a = []
 for i in range(n):
     a.append(int(input()))
 
-min_v = 1000000
+min_v = float('inf')  # 최소값을 찾기 위해 초기값을 무한대로 설정합니다.
 for i in range(n):
-    start = i
     sum_v = 0
     for j in range(1, n):
-        start = (i+j) % 5
-        sum_v += a[start] * j
+        # 이동한 거리 j에 대해 모듈러 연산을 적용하여 인덱스 오버플로우를 방지합니다.
+        next_room = (i + j) % n
+        sum_v += a[next_room] * j
     min_v = min(min_v, sum_v)
 
 print(min_v)
