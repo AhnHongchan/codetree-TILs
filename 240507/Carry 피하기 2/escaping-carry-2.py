25e924dabc0b2419508939dca2f7d7db10c11dfa
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-07 02:24:12 +0900

    Title: Carry 피하기 2, Time: 116ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240507/Carry 피하기 2/escaping-carry-2.py
@@ -0,0 +1,26 @@
+n = int(input())
+code = []
+for i in range(n):
+    code.append(int(input()))
+
+ans = -1
+for i in range(n-2):
+    for j in range(i+1, n-1):
+        for k in range(n):
+            carry = False
+            a, b, c = code[i], code[j], code[k]
+            while a > 0 or b > 0 or c > 0:
+                if a % 10 + b % 10 + c % 10 < 10:
+                    if a > 0:
+                        a //= 10
+                    if b > 0:
+                        b //= 10
+                    if c > 0:
+                        c //= 10
+                else:
+                    carry = True
+                    break
+            if not carry:
+                cnt = code[i] + code[j] + code[k]
+                ans = max(ans, cnt)
+print(ans)
