ac439390ab6dac74855579b86afa69e7f1a73eb0
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-07 17:25:18 +0900

    Title: 바구니 안의 사탕 2, Time: 117ms, Memory: 25MB, Status: Passed - CodeTree

--- 240507/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240507/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,11 +1,18 @@
-n, k = map(int, input().split())
-arr = [0] * 101
-for _ in range(n):
-    candy, idx = map(int, input().split())
-    arr[idx] = candy
+# 입력 받기
+N, K = map(int, input().split())
+buckets = []
+for _ in range(N):
+    candies, position = map(int, input().split())
+    buckets.append((candies, position))
 
-ans = 0
-for i in range(101-2*k):
-    val = sum(arr[i:i+2*k+1])
-    ans = max(ans, val)
-print(ans)
+# 사탕의 수가 최대가 되는 경우 찾기
+max_candies = 0
+for center in range(101):
+    candies_sum = 0
+    for candies, position in buckets:
+        if abs(center - position) <= K:
+            candies_sum += candies
+    max_candies = max(max_candies, candies_sum)
+
+# 결과 출력
+print(max_candies)
