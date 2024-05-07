6ca253eedf83bd25466444f3f1a1090e921de8e6
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-07 15:50:34 +0900

    Title: 최고의 13위치 2, Time: 225ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/최고의 13위치 2/best-place-of-13-2.py
+++ 240507/최고의 13위치 2/best-place-of-13-2.py
@@ -6,9 +6,7 @@ ans = 0
 # 1 * 3 크기의 격자를 가로로 배치하는 경우
 for x in range(n):
     for y in range(n - 2):
-        cnt = 0
-        for z in range(3):
-            cnt += arr[x][y + z]
+        cnt = sum(arr[x][y:y+3])
 
         # 두 번째 격자를 배치하는 경우
         for a in range(n):
@@ -16,9 +14,7 @@ for x in range(n):
                 if a == x and (b == y or b == y + 1 or b == y + 2):  # 겹치지 않게 조건 추가
                     continue
                 else:
-                    cnt2 = 0
-                    for z in range(3):
-                        cnt2 += arr[a][b + z]
+                    cnt2 = sum(arr[a][b:b+3])
                     val = cnt + cnt2
                     ans = max(ans, val)
 
