2d1abcf8dcde62da1d4de4a8c16f8ef390bbd0b9
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-28 00:03:49 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 164ms, Memory: 29MB, Status: Passed - CodeTree

--- 240528/한 가지로 열리는 자물쇠/one-way-lock.py
+++ 240528/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,23 +1,14 @@
 n = int(input())
-order = list(map(int, input().split()))
+a, b, c = map(int, input().split())
 exs = []
 
-ans = n ** 3
+cnt = 0
 
 for i in range(1,n+1):
     for j in range(1,n+1):
         for k in range(1,n+1):
-            case = [i, j, k]
-            exs.append(case)
+            if abs(a - i) <= 2 or abs(b - j) <= 2 or \
+               abs(c - k) <= 2:
+                cnt += 1
 
-
-for ex in exs:
-    cnt = 0
-    for m in range(3):
-        judge = abs(ex[m]-order[m])
-        if judge > 2:
-            cnt += 1
-    if cnt == 3:
-        ans -= 1
-
-print(ans)
+print(cnt)
