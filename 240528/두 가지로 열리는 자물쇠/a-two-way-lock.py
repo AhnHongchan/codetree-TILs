d94834eef3e8b61def4d83d3b6c1ef57ed2806f8
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-28 23:57:24 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 231ms, Memory: 26MB, Status: Passed - CodeTree

--- 240528/두 가지로 열리는 자물쇠/a-two-way-lock.py
+++ 240528/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -1,21 +1,22 @@
-def get_nearby_numbers(n, num):
-    return [(num + i - 1) % n + 1 for i in range(-2, 3)]
-
-def get_all_combinations(n, a, b, c):
-    combinations = set()
-    for i in get_nearby_numbers(n, a):
-        for j in get_nearby_numbers(n, b):
-            for k in get_nearby_numbers(n, c):
-                combinations.add((i, j, k))
-    return combinations
-
+# 변수 선언 및 입력
 n = int(input())
-a1, b1, c1 = map(int, input().split())
-a2, b2, c2 = map(int, input().split())
+a, b, c = tuple(map(int, input().split()))
+a2, b2, c2 = tuple(map(int, input().split()))
 
-combinations1 = get_all_combinations(n, a1, b1, c1)
-combinations2 = get_all_combinations(n, a2, b2, c2)
 
-all_combinations = combinations1.union(combinations2)
+# 모든 조합을 다 만들어 봅니다.
+cnt = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for k in range(1, n + 1):
+            # 모든 자리가 주어진 첫 조합과의 거리가 2 이내인지 확인합니다.
+            if (abs(a - i) <= 2 or abs(a - i) >= n - 2) and (abs(b - j) <= 2 or abs(b - j) >= n - 2) and \
+               (abs(c - k) <= 2 or abs(c - k) >= n - 2):
+                cnt += 1
+			
+			# 모든 자리가 주어진 두 번째 조합과의 거리가 2 이내인지 확인합니다.
+            elif (abs(a2 - i) <= 2 or abs(a2 - i) >= n - 2) and (abs(b2 - j) <= 2 or abs(b2 - j) >= n - 2) and \
+               (abs(c2 - k) <= 2 or abs(c2 - k) >= n - 2):
+                cnt += 1
 
-print(len(all_combinations))
+print(cnt)
