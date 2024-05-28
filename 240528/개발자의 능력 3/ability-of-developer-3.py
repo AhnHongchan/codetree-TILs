fc72d15a99722d7fd960cf91fb8a46aecbd5da4a
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-05-28 23:29:32 +0900

    Title: 개발자의 능력 3, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240528/개발자의 능력 3/ability-of-developer-3.py
+++ 240528/개발자의 능력 3/ability-of-developer-3.py
@@ -1,14 +1,15 @@
 numbers = list(map(int, input().split()))
 n = len(numbers)
 
-def get_diff(i,j):
-    sum1 = numbers[i] + numbers[j]
+def get_diff(i, j, k):
+    sum1 = numbers[i] + numbers[j] + numbers[k]
     sum2 = sum(numbers) - sum1
     return abs(sum1 - sum2)
 
 min_diff = 6000000
 for i in range(n):
     for j in range(i+1, n):
-        min_diff = min(min_diff, get_diff(i, j))
+        for k in range(j+1, n):
+            min_diff = min(min_diff, get_diff(i, j, k))
 
 print(min_diff)
