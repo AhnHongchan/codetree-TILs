8a9fb22839830eb78828d8905a8cdae8edf6108d
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-06-12 00:53:22 +0900

    Title: 숫자 카운트, Time: 192ms, Memory: 25MB, Status: Passed - CodeTree

--- 240612/숫자 카운트/numeric-count.py
+++ 240612/숫자 카운트/numeric-count.py
@@ -1,44 +1,49 @@
-def calculate_counts(a, b):
-    count1 = 0  # 1번 카운트
-    count2 = 0  # 2번 카운트
-    for i in range(3):
-        if a[i] == b[i]:
-            count1 += 1
-        elif a[i] in b:
-            count2 += 1
-    return count1, count2
-
-def is_possible_number(possible_num, queries):
-    for query in queries:
-        b = query[0]
-        expected_count1 = query[1]
-        expected_count2 = query[2]
-        
-        count1, count2 = calculate_counts(possible_num, b)
-        if count1 != expected_count1 or count2 != expected_count2:
-            return False
-    return True
-
-def find_possible_numbers(n, queries):
-    from itertools import permutations
-    
-    all_possible_numbers = [''.join(p) for p in permutations('123456789', 3)]
-    possible_numbers = []
-
-    for num in all_possible_numbers:
-        if is_possible_number(num, queries):
-            possible_numbers.append(num)
-    
-    return len(possible_numbers)
-
-# 입력 받기
+# 변수 선언 및 입력
 n = int(input())
-queries = []
+arr = [
+	tuple(map(int, input().split()))
+	for _ in range(n)
+]
 
-for _ in range(n):
-    b, count1, count2 = input().split()
-    queries.append((b, int(count1), int(count2)))
+# 모든 숫자를 다 만들어 봅니다. (백의 자리수가 i, 십의 자리수가 j, 일의 자리수가 k)
+cnt = 0
+for i in range(1, 10):
+	for j in range(1, 10):
+		for k in range(1, 10):
+			# 같은 수가 있는지 확인합니다.
+			if i == j or j == k or k == i:
+				continue
+			
+			# 해당 숫자가 정답일때, 모든 입력에 대해 올바른 답이 나왔는지 확인합니다.
+			succeeded = True
+			for a, num_cnt1, num_cnt2 in arr:
+				# x : a[q]의 백의 자릿수, y : 십의 자릿수, z : 일의 자릿수
+				x = a // 100
+				y = a // 10 % 10
+				z = a % 10
+				
+				# cnt1 : 1번 카운트, cnt2 : 2번 카운트
+				cnt1 = 0
+				cnt2 = 0
+				if x == i:
+					cnt1 += 1
+				if y == j:
+					cnt1 += 1
+				if z == k:
+					cnt1 += 1
+				if x == j or x == k:
+					cnt2 += 1
+				if y == i or y == k:
+					cnt2 += 1
+				if z == i or z == j:
+					cnt2 += 1
 
-# 가능한 숫자 찾기
-result = find_possible_numbers(n, queries)
-print(result)
+				# 만약 카운트 수가 다르다면 해당 숫자는 정답이 될 수 없습니다.
+				if cnt1 != num_cnt1 or cnt2 != num_cnt2:
+					succeeded = False
+					break
+			
+			if succeeded:
+				cnt += 1
+    
+print(cnt)
