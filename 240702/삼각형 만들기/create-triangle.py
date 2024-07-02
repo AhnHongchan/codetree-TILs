1292fb417e411992692a14b1280f181a938931cc
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-07-02 15:40:16 +0900

    Title: 삼각형 만들기, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240702/삼각형 만들기/create-triangle.py
+++ 240702/삼각형 만들기/create-triangle.py
@@ -8,9 +8,9 @@ for _ in range(n):
 ans = 0
 for i in range(n):
     x1, y1 = location[i]
-    for j in range(i+1, n):
+    for j in range(n):
         x2, y2 = location[j]
-        for k in range(j+1, n):
+        for k in range(n):
             x3, y3 = location[k]
             tri = abs((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3))
             ans = max(ans, tri)
