4c0a4c1fad336bdfb983e0e660fd4965a8d8d20e
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-07-03 17:07:32 +0900

    Title: 흥미로운 숫자 2, Time: 302ms, Memory: 25MB, Status: Passed - CodeTree

--- 240703/흥미로운 숫자 2/interesting-numbers-2.py
+++ 240703/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,17 +1,20 @@
-from collections import Counter
-
 min_v, max_v = map(int, input().split())
 
 ans = 0
-for num in range(min_v, max_v+1):
-    cnt = []
-    while num:
-        a = num % 10
-        cnt.append(a)
-        num //= 10
-    element_counts = Counter(cnt)
-    for element, count in element_counts.items():
-        if count == len(cnt)-1:
+
+for num in range(min_v, max_v + 1):
+    str_num = str(num)
+    digit_count = {}
+    
+    for digit in str_num:
+        if digit in digit_count:
+            digit_count[digit] += 1
+        else:
+            digit_count[digit] = 1
+
+    for count in digit_count.values():
+        if count == len(str_num) - 1:
             ans += 1
+            break
 
 print(ans)
