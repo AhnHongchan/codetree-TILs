4e8036182d8eff23e6bbd2b221848539efe732a9
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-07-03 16:42:11 +0900

    Title: 데이터센터의 온도 조정 2, Time: 158ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240703/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
+++ 240703/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -1,13 +1,16 @@
 n, c, g, h = map(int, input().split())
 standards = [tuple(map(int, input().split())) for _ in range(n)]
 
+# 기준 값 범위 추출
+min_num = min(low for low, high in standards)
+max_num = max(high for low, high in standards)
+
 ans = 0
-for num in range(1001):
+for num in range(min_num - 1, max_num + 1):
     cnt = 0
-    for standard in standards:
-        low, high = standard
+    for low, high in standards:
         if num < low:
-           cnt += c
+            cnt += c
         elif low <= num < high:
             cnt += g
         else:
