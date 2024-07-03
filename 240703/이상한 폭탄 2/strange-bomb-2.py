758c1328b807d94c987926030355a49906c8f7d1
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-07-03 10:54:01 +0900

    Title: 이상한 폭탄 2, Time: 145ms, Memory: 25MB, Status: Passed - CodeTree

--- 240703/이상한 폭탄 2/strange-bomb-2.py
+++ 240703/이상한 폭탄 2/strange-bomb-2.py
@@ -3,10 +3,10 @@ n, k = map(int, input().split())
 bombs = [int(input()) for _ in range(n)]
 fire = []
 
-for i in range(n-3):
+for i in range(n):
     bomb = bombs[i]
     for j in range(i+1, i+k+1):
-        if bomb == bombs[j]:
+        if j < n and bomb == bombs[j]:
             fire.append(bomb)
 
 
