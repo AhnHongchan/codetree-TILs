54b5258bc6058b83338ec055f16d4ab50849f7dd
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-07-04 18:37:16 +0900

    Title: 빙산의 일각 2, Time: 96ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240704/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240704/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -1,20 +1,22 @@
-n = int(input())
+n = 7
+ice = [6, 4, 5, 2, 7, 1, 8]
+water_level = 5
 
-# 빙산 값
+submerged = [height > water_level for height in ice]
+print(submerged)  # Output: [True, False, False, False, True, False, True]
 
-ice = [int(input()) for _ in range(n)]
+def count_ice_chunks(ice, water_level):
+    submerged = [height > water_level for height in ice]
+    count = 0
+    i = 0
+    while i < n:
+        if submerged[i]:
+            count += 1
+            while i < n and submerged[i]:
+                i += 1
+        i += 1
+    return count
 
-ans = 0
-for i in range(1, 1001):
-    rest_ice = [x-i for x in ice]
-    cnt = 0
-    for j in range(n):
-        if j != n-1:
-            if rest_ice[j] > 0 and rest_ice[j+1] <=0 and j+1 < n:
-                cnt += 1
-        else:
-            if rest_ice[j-1] <= 0 and rest_ice[j] > 0:
-                cnt += 1
-    ans = max(ans, cnt)
-
-print(ans)
+# Example usage
+water_level = 5
+print(count_ice_chunks(ice, water_level))  # Output for water_level = 5
