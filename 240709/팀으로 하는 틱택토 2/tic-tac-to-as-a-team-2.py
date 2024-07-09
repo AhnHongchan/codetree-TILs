fb6c0301b89e301fc32ea95e625cfcdd76597876
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-07-09 16:59:24 +0900

    Title: 팀으로 하는 틱택토 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240709/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 240709/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,26 +1,27 @@
 def count_team_wins(board):
     # 모든 가능한 줄을 저장
     lines = [
-        # 가로줄
         board[0],
         board[1],
         board[2],
-        # 세로줄
         board[0][0] + board[1][0] + board[2][0],
         board[0][1] + board[1][1] + board[2][1],
         board[0][2] + board[1][2] + board[2][2],
-        # 대각선
         board[0][0] + board[1][1] + board[2][2],
         board[0][2] + board[1][1] + board[2][0]
     ]
 
     team_win_count = 0
+    checked_teams = set()
 
     # 모든 줄을 확인하여 팀 승리 가능한 줄의 수를 계산
     for line in lines:
         unique_numbers = set(line)
         if len(unique_numbers) == 2:
-            team_win_count += 1
+            team = tuple(sorted(unique_numbers))
+            if team not in checked_teams:
+                checked_teams.add(team)
+                team_win_count += 1
 
     return team_win_count
 
