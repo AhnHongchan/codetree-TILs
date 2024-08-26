372f88256120c26ecafc0a014b7f671bd2c7f814
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-08-26 16:17:33 +0900

    Title: 독서실의 거리두기 5, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240826/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240826/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,24 +1,27 @@
-def max_length(seats):
-    result = 0
-    for i in range(len(seats)):
-        seat = i
-        if seats[i] == 1:
-            continue
-        else:
-            my_seats = seats[:]
-            my_seats[seat] = 1
-            ans = 100000
-            cnt = 1
-            for j in range(len(my_seats)):
-                if j == 1:
-                    ans = min(ans, cnt)
-                    cnt = 1
-                else:
-                    cnt += 1
-        result = max(result, ans)
+def get_max_distance(seats):
+    max_distance = 0
 
-    return result
+    for i in range(len(seats)):
+        if seats[i] == '0':  # 빈 자리만 고려
+            seats[i] = '1'  # 사람을 이 자리에 앉힌다
+            
+            # 거리 계산
+            last_person = -1
+            min_distance = len(seats)  # 현재 가능한 최소 거리를 최대값으로 초기화
+            for j in range(len(seats)):
+                if seats[j] == '1':
+                    if last_person != -1:
+                        min_distance = min(min_distance, j - last_person)
+                    last_person = j
+
+            max_distance = max(max_distance, min_distance)  # 최대 최소 거리를 업데이트
+
+            seats[i] = 0  # 원래 상태로 복구
+
+    return max_distance
+
 n = int(input())
 seats = list(input())
 
-print(max_length(seats))
+result = get_max_distance(seats)
+print(result)
