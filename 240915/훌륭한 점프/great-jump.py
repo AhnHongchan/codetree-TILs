ed86962bbe2c6cfd50462f6aba6bd56989cd5f5d
AhnHongchan <156401196+AhnHongchan@users.noreply.github.com>
2024-09-15 04:43:00 +0900

    Title: 훌륭한 점프, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240915/훌륭한 점프/great-jump.py
@@ -0,0 +1,21 @@
+def min_max_value_on_path(n, k, arr):
+    # 초기값 설정
+    dp = [float('inf')] * n
+    dp[0] = arr[0]  # 1번 돌에서 시작
+
+    # 동적 프로그래밍을 통한 최소 최댓값 계산
+    for i in range(1, n):
+        # i번 돌에 도달하기 위한 이전 돌 j의 가능한 범위 (i-k 부터 i-1 까지)
+        for j in range(max(0, i - k), i):
+            print('dpj', j)
+            print(dp[j])
+            dp[i] = min(dp[i], max(dp[j], arr[i]))
+            print('dpi', i)
+            print(dp[i])
+    # 결과값 출력
+    return dp[-1]
+
+# 입력 예제
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+print(min_max_value_on_path(n, k, arr))
